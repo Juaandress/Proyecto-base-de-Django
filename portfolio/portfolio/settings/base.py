681b64a8d4ee7779bc631d4dbe37ed8c857a85a6
juaandress <juan-rcia@outlook.com>
2020-09-03 18:16:56 -0300

    v2

--- portfolio/portfolio/settings/base.py
+++ portfolio/portfolio/settings/base.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mysql_cymysql',
+        'NAME': 'bdappnotas',
+        'USER': 'root',
+        'PASSWORD': 'Argentina1996',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
@@ -119,5 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/MEDIA/'
+STATIC_FILE_DIRS = (
+    os.path.join(os.path.dirname(BASE_DIR),'static'),
+)
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR),'media')
